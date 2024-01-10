bced405e8f2359bd905e3399838df48d4dd6581f
trungTech1 <quangtrungdn1994@gmail.com>
2024-01-10 08:38:34 +0700

    update

--- cartCheck.js
+++ cartCheck.js
@@ -1,12 +1,12 @@
 let iconCart = document.querySelector(".icon-cart");
-let body = document.querySelector("body");
+let wrapper = document.querySelector(".wrapper");
 let closeTableBtn = document.querySelector(".closeTable"); // Thay đổi class này từ "close" sang "closeTable"
 
 iconCart.addEventListener("click", () => {
   console.log("hihi");
-  body.classList.toggle("showcart");
+  wrapper.classList.toggle("showcart");
 });
 
 closeTableBtn.addEventListener("click", () => {
-  body.classList.toggle("showcart");
+  wrapper.classList.toggle("showcart");
 });
